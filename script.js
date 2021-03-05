11a3ff2d3e4aefed2c13b9b7575f70ef5dc065ac
vitalygv <kewl.dkmr@gmail.com>
2021-03-05 23:26:45 +0300

    refactoring

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const add = (num1, num2) => num1 + num2;
-const subtract = (num1, num2) => num1 - num2;
-const multiply = (num1, num2) => num1 * num2;
-const divide = (num1, num2) => num1 / num2;
+
 
 const operate = function (operation, num1, num2) {
-    if (operation == "add") { return add(num1, num2) }
-    else if (operation == "subtract") { return subtract(num1, num2) }
-    else if (operation == "multiply") { return multiply(num1, num2) }
-    else { return divide(num1, num2) };
+    const OPERATIONS = {
+        add: (num1, num2) => num1 + num2,
+        subtract: (num1, num2) => num1 - num2,
+        multiply: (num1, num2) => num1 * num2,
+        divide: (num1, num2) => num1 / num2
+    };
+    return OPERATIONS[operation](num1, num2);
 }
 
 let outputText = document.getElementById("output");
@@ -22,19 +22,16 @@ let operationValue = "";
 let numValue1 = "";
 let numValue2 = "";
 
-function disableDot() { outputText.textContent.includes(".") ? dotButton.disabled = true : dotButton.disabled = false };
+function disableDot() { dotButton.disabled = outputText.textContent.includes(".") };
 
-removeButton.addEventListener("click", (event) => {
+removeButton.addEventListener("click", () => {
     outputText.textContent = outputText.textContent.slice(0, -1);
     displayValue = Number(outputText.textContent);
 });
 
 numButtons.forEach((button) => button.addEventListener("click", (event) => {
-    outputText.textContent == "0" ? (
-        outputText.textContent = "", outputText.textContent += event.target.textContent
-    ) : (
-            outputText.textContent += event.target.textContent
-        );
+    if (outputText.textContent == "0") outputText.textContent = "";
+    outputText.textContent += event.target.textContent;
     disableDot();
     displayValue = Number(outputText.textContent);
 }));
@@ -47,11 +44,11 @@ operationButtons.forEach((button) => button.addEventListener("click", (event) =>
 
 equalButton.addEventListener("click", () => {
     numValue2 = displayValue;
-    (numValue2 == "0" && operationValue == "divide") ? (
-        alert("DIVISION BY ZERO PROHIBITED!")
-    ) : (
-            outputText.textContent = operate(operationValue, numValue1, numValue2)
-        );
+    if (numValue2 == "0" && operationValue == "divide") {
+        alert("DIVISION BY ZERO PROHIBITED!");
+    } else {
+        outputText.textContent = operate(operationValue, numValue1, numValue2);
+    }
     displayValue = Number(outputText.textContent);
 });
 
