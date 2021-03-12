6315cc90b50e3cac8c072cb735e800ad8a276b39
vitalygv <kewl.dkmr@gmail.com>
2021-03-12 20:49:12 +0300

    Keyboard support added, new buttons layout

--- script.js
+++ script.js
@@ -25,31 +25,33 @@ let numValue2 = "";
 let repeatLastEqualsOperation = false;
 let displayValueForEqualsRepeat = "";
 
-function disableDot() { dotButton.disabled = outputText.textContent.includes(".") };
 
-removeButton.addEventListener("click", () => {
-    outputText.textContent = outputText.textContent.slice(0, -1);
-    displayValue = Number(outputText.textContent);
-});
-
-numButtons.forEach((button) => button.addEventListener("click", (event) => {
+function inputNumber(event) {
     if (outputText.textContent === "0" || repeatLastEqualsOperation) { outputText.textContent = "" };
-    outputText.textContent += event.target.textContent;
+    if (event.key === undefined) { outputText.textContent += event.target.textContent }
+    else { outputText.textContent += event.key }
     disableDot();
     displayValue = Number(outputText.textContent);
     displayValueForEqualsRepeat = displayValue;
     repeatLastEqualsOperation = false;
-}));
+}
 
-operationButtons.forEach((button) => button.addEventListener("click", (event) => {
+function disableDot() { dotButton.disabled = outputText.textContent.includes(".") };
+
+function removeSymbol() {
+    outputText.textContent = outputText.textContent.slice(0, -1);
+    displayValue = Number(outputText.textContent);
+}
+
+function selectOperation(event) {
     numValue1 = displayValue;
-    operationValue = event.target.id;
-    console.log(operationValue);
+    if (event.key === undefined) { operationValue = event.target.id }
+    else { operationValue = convertOperator(event.key) }
     outputText.textContent = "";
     repeatLastEqualsOperation = false;
-}));
+}
 
-equalButton.addEventListener("click", () => {
+function evaluate() {
     numValue2 = displayValue;
     if (numValue2 === 0 && operationValue === "divide") {
         alert("DIVISION BY ZERO PROHIBITED!");
@@ -62,20 +64,48 @@ equalButton.addEventListener("click", () => {
     }
     displayValue = Number(outputText.textContent);
     repeatLastEqualsOperation = true;
-});
+}
 
-plusMinusButton.addEventListener("click", () => {
+function convertPlusMinus() {
     if (outputText.textContent.includes("-")) {
         outputText.textContent = outputText.textContent.slice(1);
     } else {
         outputText.textContent = "-" + outputText.textContent;
     }
     displayValue = Number(outputText.textContent);
-});
+}
 
-acButton.addEventListener("click", () => {
+function clearAll() {
     numValue1 = "";
     numValue2 = "";
     outputText.textContent = "0";
     repeatLastEqualsOperation = false;
-})
+}
+function inputKeyboard(event) {
+    if (event.key >= 0 && event.key <= 9) inputNumber(event);
+    if (event.key === ".") { if (!outputText.textContent.includes(".")) { outputText.textContent += "." } };
+    if (event.key === "Backspace") removeSymbol();
+    if (event.key === "+" || event.key === "-" || event.key === "/" || event.key === "*") selectOperation(event);
+    if (event.key === "=" || event.key === "Enter") evaluate();
+    if (event.key === "Escape") clearAll();
+    if (event.key === "s") convertPlusMinus();
+}
+
+function convertOperator(keyboardOperator) {
+    if (keyboardOperator === "+") return "add";
+    if (keyboardOperator === "-") return "subtract";
+    if (keyboardOperator === "*") return "multiply";
+    if (keyboardOperator === "/") return "divide";
+
+}
+
+numButtons.forEach((button) => button.addEventListener("click", (event) => inputNumber(event)));
+operationButtons.forEach((button) => button.addEventListener("click", (event) => selectOperation(event)));
+equalButton.addEventListener("click", evaluate);
+removeButton.addEventListener("click", removeSymbol);
+plusMinusButton.addEventListener("click", convertPlusMinus);
+acButton.addEventListener("click", clearAll)
+
+window.addEventListener("keydown", (event) => {
+    inputKeyboard(event);
+});
